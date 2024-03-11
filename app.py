4aa54aa36417d82dccdf07d7e3c8ad8b32f51977
manlikeclaro <ckibii08@gmail.com>
2024-03-11 18:18:54 +0300

    added migrations with alembic

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 from Blocklist import BLOCKLIST
 from db import db
@@ -27,6 +28,7 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     api = Api(app)
 
@@ -92,8 +94,8 @@ def create_app(db_url=None):
             401,
         )
 
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
 
     api.register_blueprint(UserBlueprint)
     api.register_blueprint(StoreBlueprint)
